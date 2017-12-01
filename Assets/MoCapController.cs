204a9ed77bbfdc71b0254ed925788acbbca23914
Jonas J. Solsvik <jonas.solsvik@gmail.com>
2017-12-01 14:38:35 +0100

    did a lot of stuff to optimize and fixed the writing to file to convert files to csv from the base thing that comes from android

--- Assets/MoCapController.cs
+++ Assets/MoCapController.cs
@@ -13,6 +13,9 @@ public class MoCapController : MonoBehaviour {
     //Hack this shit together
     List<Quaternion> secondquaternionList = new List<Quaternion>();
     List<long> secondtimes = new List<long>();
+
+
+
     int counter = 0;
     bool done = false;
     Transform secondPhone;
@@ -41,7 +44,7 @@ public class MoCapController : MonoBehaviour {
             //loc = mirror.position + loc;
             //mirror.rotation = Quaternion.LookRotation(loc - mirror.position);
             //mirror.rotation = Quaternion.LookRotation(quaternionList[counter].eulerAngles);
-            counter++; counter++;
+            counter++;
         }
     }
 
@@ -56,11 +59,8 @@ public class MoCapController : MonoBehaviour {
             {
                 //Console.WriteLine(text);
                 stringList = text.Split(',', ' ');
-                for (int i = 0; i < stringList.Length; i++)
-                {
-                    quaternionList.Add(new Quaternion(float.Parse(stringList[0]), float.Parse(stringList[1]), float.Parse(stringList[2]), float.Parse(stringList[3])));
-                    times.Add(Int64.Parse(stringList[5]));
-                }
+                quaternionList.Add(new Quaternion(float.Parse(stringList[0]), float.Parse(stringList[1]), float.Parse(stringList[2]), float.Parse(stringList[3])));
+                times.Add(Int64.Parse(stringList[5]));
             }
             else
             {
@@ -71,5 +71,57 @@ public class MoCapController : MonoBehaviour {
         done = false;
     }
 
-
+    public void convertToCSV()
+    {
+        DirectoryInfo dir = new DirectoryInfo(Application.dataPath + "/moCapData");
+        FileInfo[] info = dir.GetFiles("recordedMoCapData*.txt");
+        StreamReader read = null;
+        bool stillReading = true;
+        string text = " ";
+        string[] stringList;
+        string[] tempStrings = new string[4];
+        int count = 0;
+        List<List<string[]>> dataList = new List<List<string[]>>();
+        foreach (FileInfo f in info)
+        {
+            read = f.OpenText();
+            dataList.Add(new List<string[]>());
+            while (stillReading)
+            {
+                if ((text = read.ReadLine()) != null)
+                {
+                    stringList = text.Split(',', ' ');
+                    dataList[count].Add(stringList);
+                }
+                else
+                {
+                    stillReading = false;
+                }
+            }
+            count++;
+            stillReading = true;
+        }
+        
+        for (int i = 0; i < dataList.Count; i++)
+        {
+            string[] tempStringList = new string[dataList[i].Count];
+            for (int j = 0; j < dataList[i].Count; j++)
+            {
+                tempStringList[0] = tempStringList[0] + "," + dataList[i][j][0];
+                tempStringList[1] = tempStringList[1] + "," + dataList[i][j][1];
+                tempStringList[2] = tempStringList[2] + "," + dataList[i][j][2];
+                tempStringList[3] = tempStringList[3] + "," + dataList[i][j][3];
+            }
+            if(File.Exists("Assets/moCapData" + i + ".txt"))
+            {
+                File.Delete("Assets/moCapData" + i + ".txt");
+            }
+            //WHY WAS I STUCK ON THIS FOR AN HOUR?! Because the stupid Remove method does not manipulate the string in itself so it needs to be assigned back onto itself. I WANT POINTERS!
+            tempStringList[0] = tempStringList[0].Remove(0, 1);
+            tempStringList[1] = tempStringList[1].Remove(0, 1);
+            tempStringList[2] = tempStringList[2].Remove(0, 1);
+            tempStringList[3] = tempStringList[3].Remove(0, 1);
+            File.WriteAllLines("Assets/moCapData" + i + ".txt", tempStringList);
+        }
+    }
 }
